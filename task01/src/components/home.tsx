684ae194af47eb77b0d53dfa931a7ed33aed82c9
Pavi99sandamini151 <pavithrasandamini283@gmail.com>
2022-11-12 19:59:46 +0530

    home commit

--- task01/src/components/home.tsx
+++ task01/src/components/home.tsx
@@ -1,27 +1,31 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import {Button, Col, Container, Row} from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 const Home = () => {
     const location = useLocation();
     const navigate = useNavigate();
     return(
         <Container>
-            <Col>
-                <Row>
+            <Col className="p-0">
+                <Row className="m-0 p-2">
                     <h1>Welcome Home</h1>
                 </Row>
-                <Row>
-                    { location.state.name }
+                <Row className="m-0 p-2">
+                    <h3>
+                        { location.state.name }
+                    </h3>
                 </Row>
-                <Row>
+                <Row className="m-0 p-2">
                     <h2>Your Password is:</h2>
                 </Row>
-                <Row>
-                    { location.state.password }
+                <Row className="m-0 p-2">
+                    <h3>
+                        { location.state.password }
+                    </h3>
                 </Row>
-                <Col className="border border-2 border-red" xs={5}>
-                    <Row className="xs">
+                <Col className="p-0 m-2" xs={5}>
+                    <Row className="m-0 p-2">
                         <Button onClick={()=> navigate('/' , )}>Log Out</Button>
                     </Row>
                 </Col>
