d776aef62138a43544270501792155a3e94c5f78
Pavi99sandamini151 <pavithrasandamini283@gmail.com>
2022-11-16 13:44:43 +0530

    task03 API committed

---
+++ task03new/src/components/firstpage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Button, Col, Container, Image, Row} from "react-bootstrap";
+
+
+const Firstpage = () => {
+
+    return(
+        <Container>
+            hi
+        </Container>
+    );
+
+};
+
+export default Firstpage;
